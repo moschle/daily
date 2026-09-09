533216d67a7172a500cca633d9e1767f3c8dfc9d
moritz <6gdtfmmdh6@privaterelay.appleid.com>
2026-09-09 08:58:28 +0200

    Revert stellen_check.py to last good version (5d8e25e)

--- stellen_check.py
+++ stellen_check.py
@@ -1,1 +1,10 @@
-RESTORE_FROM_COMMIT_5d8e25e3_THEN_PATCH
+#!/usr/bin/env python3
+"""
+Stellen-Monitor: tägliche Suche nach passenden Wissenschafts-/Behörden-Stellen.
+Quellen:
+  DE: H-Soz-Kult (Atom), kultweet.de (HTML), UniBwM (HTML), service.bund.de (RSS)
+  Internationale Erweiterung: arthist.net (RSS), jobs.ac.uk × 3 Fachbereiche (RSS)
+Filter: Score-basiert mit Wortgrenzen-Matching + Hard-Blacklist.
+Output: HTML-Mail an GMAIL_ADDRESS, wenn neue Treffer da sind.
+State: stellen_seen.json mit gesehenen IDs (Aufräumen nach 90 Tagen).
+"""
